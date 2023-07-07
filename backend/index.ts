335f6cc4aa913d7bb76df4b0ea6c55a7816bca17
roy-coder <52110946+roy-coder@users.noreply.github.com>
2023-07-07 13:14:56 -0400

    Combine branches (#5)
    * Initial RDS
    
    * Changing action file
    
    * Update index.ts
    
    * Fixing db connection
    
    * Changing approach
    
    * Port fix
    
    * Defining db_name
    
    * Fixing postgress issues
    
    * Minor tweak
    
    * Destroy / Create
    
    * Destroy / Create
    
    * Destroy / Create
    
    * Bumping hatchify version
    
    * Bump Koa
    
    * Combine branches
    
    * Change branch
    
    * Bump React
    
    ---------
    
    Co-authored-by: LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>

--- backend/index.ts
+++ backend/index.ts
@@ -1,25 +1,80 @@
-// hatchify-app/backend/index.ts
+import fs from "fs";
+import { Options } from "sequelize";
+import dotenv from "dotenv";
+import { Command } from "commander";
+import Express from "express";
 import Koa from "koa";
-import cors from "@koa/cors";
+import koaCors from "@koa/cors";
+import cors from "cors";
+import { hatchifyExpress } from "@hatchifyjs/express";
 import { hatchifyKoa } from "@hatchifyjs/koa";
 import { Document } from "../schemas/Document";
 
-const app = new Koa();
-const hatchedKoa = hatchifyKoa([Document], {
+dotenv.config({ path: "../.env" });
+
+const options = new Command()
+  .requiredOption("-f, --framework <express|koa>", "Node framework")
+  .requiredOption("-d, --database <sqlite|postgres>", "Database type")
+  .parse()
+  .opts();
+
+const hatchedNode = getHatchFunction(options.framework)([Document], {
   prefix: "/api",
-  database: {
-    dialect: "sqlite",
-    storage: "example.sqlite",
-  },
+  database: getDatabaseConfiguration(options.database),
 });
 
-app.use(cors());
-app.use(hatchedKoa.middleware.allModels.all);
-
 (async () => {
-  await hatchedKoa.createDatabase();
+  await hatchedNode.createDatabase();
 
-  app.listen(3000, () => {
+  setupApp(hatchedNode.middleware.allModels.all).listen(3000, () => {
     console.log("Started on port 3000");
   });
 })();
+
+function getHatchFunction(framework: "express" | "koa") {
+  if (framework === "express") return hatchifyExpress;
+  return hatchifyKoa;
+}
+
+function getDatabaseConfiguration(database: "postgres" | "sqlite"): Options {
+  if (database === "postgres")
+    return {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false,
+          ca: [fs.readFileSync(__dirname + "/../rds-combined-ca-bundle.pem")],
+        },
+      },
+      host: process.env.PGHOST,
+      port: Number(process.env.PGPORT),
+      ssl: true,
+      database: process.env.PGDATABASE,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+    };
+
+  return {
+    dialect: "sqlite",
+    storage: "example.sqlite",
+  };
+}
+
+function setupApp(middleware: any) {
+  if (options.framework === "express") return setupExpress(middleware);
+  return setupKoa(middleware);
+}
+
+function setupExpress(middleware: any) {
+  const app = Express();
+  app.use(cors());
+  app.use(middleware);
+  return app;
+}
+
+function setupKoa(middleware: any) {
+  const app = new Koa();
+  app.use(koaCors());
+  app.use(middleware);
+  return app;
+}
