f6f1c3369a7545e6b9725a59987959f135fb34ce
Christina Labay <christina@bitovi.com>
2023-07-18 14:35:34 -0500

    add action column to grid

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -6,6 +6,7 @@ import {
 } from "@hatchifyjs/react";
 import { Document } from "../schemas/Document";
 const backend_url = import.meta.env.VITE_BACKEND_URL;
+import { Icon } from "@mui/material";
 
 export const hatchedReact = hatchifyReact(
   { Document },
@@ -15,11 +16,31 @@ export const hatchedReact = hatchifyReact(
 );
 
 const DocumentList = hatchedReact.components.Document.List;
+const DocumentExtraColumn = hatchedReact.components.Document.ExtraColumn;
+const DocumentEmptyList = hatchedReact.components.Document.EmptyList;
 
 const App: React.FC = () => {
   return (
     <MuiProvider>
-      <DocumentList />
+      <DocumentList>
+        <DocumentEmptyList>
+          <div onClick={() => console.log("record")}>
+            {"No records to display"}
+          </div>
+        </DocumentEmptyList>
+        <DocumentExtraColumn
+          label="Action"
+          render={({ record }) => {
+            return (
+              <div onClick={() => console.log(record)}>
+                <Icon className="material-icons" sx={{ color: "grey" }}>
+                  visibility
+                </Icon>
+              </div>
+            );
+          }}
+        />
+      </DocumentList>
     </MuiProvider>
   );
 };
