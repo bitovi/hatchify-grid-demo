b67364198e5f5a0b20585e906b396788868cbf28
Arthur Pankiewicz <arthur@bitovi.com>
2023-07-20 14:26:35 -0700

    style grid to look similar to figma

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,24 @@
 // hatchify-app/frontend/App.tsx
+import { useState } from "react";
+import { createTheme } from "@mui/material";
 import {
   hatchifyReact,
   MuiProvider,
   createJsonapiClient,
 } from "@hatchifyjs/react";
 import { Document } from "../schemas/Document";
+import {
+  DocumentStatus,
+  DocumentActions,
+  DocumentDate,
+  FiltersRow,
+} from "./components/DocumentTable";
+
+// @ts-expect-error
 const backend_url = import.meta.env.VITE_BACKEND_URL;
-import { IconButton } from "@mui/material";
 
 export const hatchedReact = hatchifyReact(
+  // @ts-expect-error
   { Document },
   createJsonapiClient(backend_url, {
     Document: { endpoint: "documents" },
@@ -17,32 +27,54 @@ export const hatchedReact = hatchifyReact(
 
 const DocumentList = hatchedReact.components.Document.List;
 const DocumentExtraColumn = hatchedReact.components.Document.ExtraColumn;
+const DocumentColumn = hatchedReact.components.Document.Column;
 const DocumentEmptyList = hatchedReact.components.Document.EmptyList;
 
 const App: React.FC = () => {
+  const [selected, setSelected] = useState<string[]>([]);
+
   return (
-    <MuiProvider>
-      <DocumentList>
-        <DocumentEmptyList>
-          <div>{"No records to display"}</div>
-        </DocumentEmptyList>
-        <DocumentExtraColumn
-          label="Action"
-          render={({ record }) => {
-            return (
-              <IconButton
-                className="material-icons"
-                sx={{ color: "grey" }}
-                onClick={() => console.log(record)}
-              >
-                visibility
-              </IconButton>
-            );
-          }}
-        />
+    <MuiProvider theme={theme}>
+      <FiltersRow selected={selected} />
+      <DocumentList
+        selectable
+        onSelectionChange={(selected) => setSelected(selected)}
+      >
+        <DocumentColumn attribute="name" />
+        <DocumentColumn attribute="date" renderValue={DocumentDate} />
+        <DocumentColumn attribute="status" ValueComponent={DocumentStatus} />
+        <DocumentExtraColumn label="Action" ValueComponent={DocumentActions} />
+        <DocumentEmptyList>No records to display</DocumentEmptyList>
       </DocumentList>
     </MuiProvider>
   );
 };
 
 export default App;
+
+const theme = createTheme({
+  components: {
+    MuiTableHead: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#F4F6F8",
+          "& th": {
+            color: "#818D96",
+          },
+        },
+      },
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          "&:nth-child(1)": {
+            width: 40,
+          },
+          "&:last-child": {
+            textAlign: "right",
+          },
+        },
+      },
+    },
+  },
+});
