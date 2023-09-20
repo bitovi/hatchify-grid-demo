49e374d2d82ed4bd93624e29f87cec379b9c1146
Roye <roye@bitovi.com>
2023-09-20 08:11:57 -0400

    cleanup

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -20,7 +20,7 @@ const backend_url =
   import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api";
 
 export const hatchedReact = hatchifyReact(
-  { Document: v2ToV1(Document) },
+  v2ToV1({ Document }),
   createJsonapiClient(backend_url, {
     Document: { endpoint: "documents" },
   })
--- schemas/v2ToV1.ts
+++ schemas/v2ToV1.ts
@@ -1,21 +1,57 @@
-import { PartialSchema } from "@hatchifyjs/hatchify-core";
+import { PartialSchema, assembler } from "@hatchifyjs/hatchify-core";
 
-export function v2ToV1(schema: PartialSchema) {
-  return {
-    name: schema.name,
-    attributes: Object.entries(schema.attributes).reduce(
-      (acc, [key, value]) => ({
-        ...acc,
-        [key]: {
-          type: value.orm.sequelize.type,
-          allowNull: value.orm.sequelize.allowNull,
-          values:
-            value.orm.sequelize.type === "ENUM" &&
-            "typeArgs" in value.orm.sequelize &&
-            value.orm.sequelize.typeArgs,
-        },
-      }),
-      {}
-    ),
-  };
+export function v2ToV1(schemas: Record<string, PartialSchema>) {
+  return Object.entries(assembler(schemas)).reduce(
+    (schemaAcc, [schemaName, { relationships, ...schema }]) => ({
+      ...schemaAcc,
+      [schemaName]: {
+        ...schema,
+        attributes: Object.entries(schema.attributes).reduce(
+          (acc, [key, { orm }]) => ({
+            ...acc,
+            [key]: {
+              ...orm.sequelize,
+              values:
+                orm.sequelize.type === "ENUM" &&
+                "typeArgs" in orm.sequelize &&
+                orm.sequelize.typeArgs,
+            },
+          }),
+          {}
+        ),
+        ...Object.entries(relationships ?? {}).reduce(
+          (relationshipAcc, [relationshipName, relationship]) => {
+            const type =
+              relationship.type === "hasManyThrough"
+                ? "belongsToMany"
+                : relationship.type;
+            return {
+              ...relationshipAcc,
+              [type]: [
+                ...relationshipAcc[type],
+                {
+                  target: relationship.targetSchema,
+                  options: {
+                    as: relationshipName,
+                    foreignKey:
+                      ("sourceAttribute" in relationship &&
+                        relationship.sourceAttribute) ||
+                      ("targetAttribute" in relationship &&
+                        relationship.targetAttribute),
+                  },
+                },
+              ],
+            };
+          },
+          {
+            belongsTo: [],
+            belongsToMany: [],
+            hasMany: [],
+            hasOne: [],
+          }
+        ),
+      },
+    }),
+    {}
+  );
 }
